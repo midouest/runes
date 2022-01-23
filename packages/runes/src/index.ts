7e9aede7dc09a08d1f9ba0137b16c3e903b01128
Robin <midouesterner@gmail.com>
2022-01-23 12:46:53 -0600

    add preload script

--- packages/runes/src/index.ts
+++ packages/runes/src/index.ts
@@ -6,6 +6,7 @@ import { app, BrowserWindow, session } from "electron";
 // plugin that tells the Electron app where to look for the Webpack-bundled app code (depending on
 // whether you're running in development or production).
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
+declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -19,6 +20,9 @@ const createWindow = (): void => {
     height: 600,
     width: 800,
     show: false,
+    webPreferences: {
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+    },
   });
 
   // and load the index.html of the app.
