8dad258ee0def5ef280cb30106c55a23b6ad35fe
Robin <midouesterner@gmail.com>
2022-02-01 06:53:54 -0600

    remove hard-coded dev tools extension versions

---
+++ src/backend/extensions.ts
@@ -0,0 +1,37 @@
+import * as os from "os";
+import * as path from "path";
+import * as fs from "fs/promises";
+
+import { session } from "electron";
+
+const EXTENSIONS = {
+  "React Dev Tools": "fmkadmapgofadopljbjfkapdkoienihi",
+  "C/C++ Dev Tools": "pdcpmagijalfljmkmjngeonclgbbannb",
+} as const;
+
+const BASE_EXTENSION_PATH = path.join(
+  os.homedir(),
+  "/Library/Application Support/Google/Chrome/Default/Extensions"
+);
+
+async function getExtensionPath(extensionId: string): Promise<string> {
+  const extDir = path.join(BASE_EXTENSION_PATH, extensionId);
+  const files = await fs.readdir(extDir);
+  return path.join(extDir, files[0]);
+}
+
+async function loadExtension(extensionId: string): Promise<void> {
+  const extensionPath = await getExtensionPath(extensionId);
+  await session.defaultSession.loadExtension(extensionPath);
+}
+
+export async function loadExtensions(): Promise<void> {
+  for (const [name, id] of Object.entries(EXTENSIONS)) {
+    try {
+      await loadExtension(id);
+    } catch (err) {
+      console.log(`Failed to load ${name} extension`);
+      console.log(err);
+    }
+  }
+}
