6c040156c65cbc9f6505c10020dabad0261dc1f8
Robin <midouesterner@gmail.com>
2022-02-01 06:24:32 -0600

    remove electron-forge variable logging

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,6 @@ import { MatronClient } from "./backend/matron";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
-console.log("MAIN_WINDOW_WEBPACK_ENTRY", MAIN_WINDOW_WEBPACK_ENTRY);
-console.log(
-  "MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY",
-  MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
-);
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   // eslint-disable-line global-require
