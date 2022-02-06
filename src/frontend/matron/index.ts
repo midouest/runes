6369855aa8bacc7e20e6792c21fd185726a2c75e
Robin <midouesterner@gmail.com>
2022-02-06 10:26:08 -0600

    remove unused loadmatron hook

--- src/frontend/matron/index.ts
+++ src/frontend/matron/index.ts
@@ -1,6 +1,5 @@
 export { Matron } from "./Matron";
 export { Provider } from "./Provider";
 export { useMatron } from "./useMatron";
-export { useLoadMatron } from "./useLoadMatron";
 export { MatronWorker } from "./MatronWorker";
 export { useStartMatronWorker } from "./useStartMatronWorker";
