5d5e809a947989d7676c0e08602a6814fc64c1d1
Robin <midouesterner@gmail.com>
2022-02-12 00:54:47 -0600

    Updates

--- packages/web/src/matron/useMatron.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-
-import { Context } from "./Context";
-import { MatronWorker } from "./MatronWorker";
-
-export function useMatron(): MatronWorker | null {
-  return useContext(Context);
-}
